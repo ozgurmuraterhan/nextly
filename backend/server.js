517f62ed50830c97a0e1184e61205e9e2e259b62
ozgurmuraterhan <33369670+ozgurmuraterhan@users.noreply.github.com>
2022-03-06 18:02:37 +0300

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(mongoSanitize());
 app.use(compression());
 app.use(cookieParser());
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
 
 app.use(express.json({ limit: "1gb", parameterLimit: 50000 }));
 app.use(
