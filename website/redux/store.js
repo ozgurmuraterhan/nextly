5edc8e1ed893865eca79a37fb937a16f14168828
ozgurmuraterhan <33369670+ozgurmuraterhan@users.noreply.github.com>
2022-03-15 02:47:39 +0300

    fix chrome redux-devtools-extension

--- website/redux/store.js
+++ website/redux/store.js
@@ -4,10 +4,7 @@ import rootReducer from "./reducers/index";
 import { HYDRATE, createWrapper } from "next-redux-wrapper";
 
 const bindMiddleware = (middleware) => {
-  if (process.env.NODE_ENV == 'development') {
-    const { composeWithDevTools } = require("redux-devtools-extension");
-    return composeWithDevTools(applyMiddleware(...middleware));
-  }
+
   return applyMiddleware(...middleware);
 };
 const reducer = (state, action) => {
